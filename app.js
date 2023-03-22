7bd43c6b5a40e2bd18d4696c783b2359fd202a9f
arielxct <campostula@gmail.com>
2023-03-21 23:34:31 -0300

    Modifico App y archivo2 usando modeule.export y require

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ var barrio = "Palermo";
 var rio = " Carapachay";
 console.log (rio);
 console.log (" archivo App.js");
+// Metodo Require
+const ariel=require("./archivo2"); // importo el array creado en ariel.js
+console.log(ariel);// muestro el array completo
+console.log("Naci en la ciudad de ", ariel[1]," ,vivo en el barrio de ", ariel[0]," y me gusta ", ariel[2]);//concateno el array
